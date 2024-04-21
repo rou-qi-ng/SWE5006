8e1043c808fd9edcc50084c615b671669f335d8e
Ng Rou Qi <rouqi.ng@ncs.com.sg>
2024-04-21 22:51:22 +0800

    Service Facade

---
+++ beautyApp/backend/src/main/java/com/example/beautyApp/facade/ServiceFacade.java
@@ -0,0 +1,52 @@
+package com.example.beautyApp.facade;
+import org.springframework.stereotype.Component;
+
+import com.example.beautyApp.manager.ReviewManager;
+import com.example.beautyApp.manager.ServiceProfileManager;
+import com.example.beautyApp.model.Portfolio;
+import com.example.beautyApp.model.Pricing;
+import com.example.beautyApp.model.Review;
+import com.example.beautyApp.model.ServiceProfile;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class ServiceFacade {
+    private ServiceProfileManager serviceProfileManager;
+    private ReviewManager reviewManager;
+
+    public ServiceFacade(ServiceProfileManager serviceProfileManager, ReviewManager reviewManager) {
+        this.serviceProfileManager = serviceProfileManager;
+        this.reviewManager = reviewManager;
+    }
+
+    public List<Portfolio> getAllImagesByServiceId(int serviceId) {
+        return serviceProfileManager.getAllImagesByServiceId(serviceId);
+    }
+
+    public List<Portfolio> getFirstLogoByServiceId(int serviceId) {
+        return serviceProfileManager.getFirstLogoByServiceId(serviceId);
+    }
+
+    public List<Portfolio> getPortfolioImagesByServiceId(int serviceId) {
+        return serviceProfileManager.getPortfolioImagesByServiceId(serviceId);
+    }
+
+    public List<Pricing> getAllPricingsByServiceId(int serviceId) {
+        return serviceProfileManager.getAllPricingsByServiceId(serviceId);
+    }
+
+    public List<Review> getAllReviewsByServiceId(int serviceId) {
+        return serviceProfileManager.getAllReviewsByServiceId(serviceId);
+    }
+    
+    public Optional<ServiceProfile> getServiceProfileById(int serviceId) {
+        return serviceProfileManager.getServiceProfileById(serviceId);
+    }
+
+    public void saveReview(Review reviewData) {
+        reviewManager.save(reviewData);
+    }
+
+}
